ca45ec55eedd44a6945a2fdc9b43e26b4e458441
leonardo cruz <leonardo.m.cruz@hotmail.com>
2021-11-23 21:27:55 -0300

    Modelos iniciais adicionados

--- TesteBackendEnContact.Core/Class1.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace TesteBackendEnContact.Core
-{
-    public class Class1
-    {
-        public int Campo { get; set; }
-    }
-}
---
+++ TesteBackendEnContact.Core/Models/ContactBook.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace TesteBackendEnContact.Core.Models
+{
+    public class ContactBook
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string UserId { get; set; }
+        public ICollection<Contact> Contacts { get; set; }
+    }
+}
