12ec9583cb52d4ddfb27625ffdd22f7b2edc0994
leonardo cruz <leonardo.m.cruz@hotmail.com>
2021-11-25 18:22:29 -0300

    Removido recurso desnecessario

--- TesteBackendEnContact.Api/Resources/Contact/SaveContactResource.cs
+++ TesteBackendEnContact.Api/Resources/Contact/SaveContactResource.cs
@@ -2,7 +2,6 @@ namespace TesteBackendEnContact.Api.Resources.Contact
 {
     public class SaveContactResource
     {
-        public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
