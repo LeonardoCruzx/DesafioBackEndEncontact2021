5fc280bd9fd40f44eed14382e687ad18e375bef7
leonardo cruz <leonardo.m.cruz@hotmail.com>
2021-11-25 17:55:02 -0300

    Adicionado recurso para salvamento de contato atraves de um csv

---
+++ TesteBackendEnContact.Api/Resources/Contact/CsvSaveContactResource.cs
@@ -0,0 +1,11 @@
+namespace TesteBackendEnContact.Api.Resources.Contact
+{
+    public class CsvSaveContactResource
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public int ContactBookId { get; set; }
+        public int CompanyId { get; set; }
+    }
+}
