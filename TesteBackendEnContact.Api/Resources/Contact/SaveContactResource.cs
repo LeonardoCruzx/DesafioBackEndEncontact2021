9d2297cf4e1135227091609529652c8fe46e868d
leonardo cruz <leonardo.m.cruz@hotmail.com>
2021-11-25 20:22:42 -0300

    Value tipes transformados em nullaveis

--- TesteBackendEnContact.Api/Resources/Contact/SaveContactResource.cs
+++ TesteBackendEnContact.Api/Resources/Contact/SaveContactResource.cs
@@ -5,7 +5,7 @@ namespace TesteBackendEnContact.Api.Resources.Contact
         public string Name { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
-        public int ContactBookId { get; set; }
-        public int CompanyId { get; set; }
+        public int? ContactBookId { get; set; }
+        public int? CompanyId { get; set; }
     }
 }
