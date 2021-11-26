1c65e0f7b0b199ee44f304498e237cdd9a586b3c
leonardo cruz <leonardo.m.cruz@hotmail.com>
2021-11-25 22:36:32 -0300

    Adicionado metodo de pesquisa de contato a interface

--- TesteBackendEnContact.Core/Interfaces/Services/ICompanyService.cs
+++ TesteBackendEnContact.Core/Interfaces/Services/ICompanyService.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using TesteBackendEnContact.Core.Filters;
 using TesteBackendEnContact.Core.Models;
 using TesteBackendEnContact.Core.Pagination;
 
@@ -10,6 +11,7 @@ namespace TesteBackendEnContact.Core.Interfaces.Services
         Task<IEnumerable<Company>> GetAllCompanies();
         Task<Paginator<Company>> GetAllCompaniesPaginated(int page = 1, int postsPerPage = 10);
         Task<Company> GetCompanyById(int id);
+        Task<Paginator<Contact>> GetContactsFromContactBookOfCompanyWithId(int companyId, int contactBookId, ContactFilter filter, int page = 1, int postsPerPage = 10);
         Task<Company> CreateCompany(Company newCompany);
         Task UpdateCompany(Company companyToBeUpdated, Company company);
         Task DeleteCompany(Company company);
