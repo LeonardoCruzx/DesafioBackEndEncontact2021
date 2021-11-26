4b38f8c26e203523a155818d298396c280ede60d
leonardo cruz <leonardo.m.cruz@hotmail.com>
2021-11-26 12:22:20 -0300

    Adicionado mapeamento

--- TesteBackendEnContact.Api/Mapping/MappingProfile.cs
+++ TesteBackendEnContact.Api/Mapping/MappingProfile.cs
@@ -2,6 +2,7 @@ using AutoMapper;
 using TesteBackendEnContact.Api.Resources;
 using TesteBackendEnContact.Api.Resources.Company;
 using TesteBackendEnContact.Api.Resources.Contact;
+using TesteBackendEnContact.Api.Resources.ContactBook;
 using TesteBackendEnContact.Core.Models;
 
 namespace TesteBackendEnContact.Api.Mapping
@@ -16,10 +17,18 @@ namespace TesteBackendEnContact.Api.Mapping
 
             CreateMap<Company, SaveCompanyResource>();
             CreateMap<SaveCompanyResource, Company>();
+
+            CreateMap<CompanyResource, SaveCompanyResource>();
+            CreateMap<SaveCompanyResource, CompanyResource>();
             #endregion
 
+            #region ContactBook
             CreateMap<ContactBook, ContactBookResource>();
             CreateMap<ContactBookResource, ContactBook>();
+
+            CreateMap<ContactBook, SaveContactBookResource>();
+            CreateMap<SaveContactBookResource, ContactBook>();
+            #endregion
 
             #region Contact
             CreateMap<Contact, ContactResource>();
